dd221a08e0c3138a77f6e441d720a794dc1d285a
tommason14 <tom.mason14@gmail.com>
2020-06-03 10:12:36 +1000

    grep lammps data

--- chem/lammps/grep_lammps_data.sh
+++ chem/lammps/grep_lammps_data.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# Grep data from all log.lammps* files.
+# Grep data from all log.lammps* files or lammps*.out.
 # Sort by timestep, the first value of the thermo_style command
 # Print to first argument, or data.csv otherwise.
 
 [ $USER == "tommason" ] || [ $USER == "tmas0023" ] && sed="gsed" || sed="sed"
 
-output=${1-data.csv}
+[ $# -eq 0 ] && echo "\
+Pass in log or lammps to take data from log.lammps* or lammps*.out
+Syntax: $(basename $0) [log|lammps] <csvfile>" && exit 1
+
+choice=$1
+output=${2-data.csv}
 echo "Writing lammps data to $output"
 
 # data- assumes Step is the first value of thermo_style
-ls log.lammps* | 
+ls "$choice"* |
   xargs $sed -n '/Step/,/Loop/p' |
   grep -v 'Step\|Loop' |
   sort -nk 1 |
@@ -18,7 +23,7 @@ ls log.lammps* |
   $sed 's/^\s\+//;s/\s\+$//;s/\s\+/,/g' > data.tmp
 
 # find the header from first output
-ls log.lammps* |
+ls "$choice"* |
   head -1 |
   xargs grep Step |
   $sed 's/^\s\+//;s/\s\+$//;s/\s\+/,/g' > header.tmp
