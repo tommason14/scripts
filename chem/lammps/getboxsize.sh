ab6e31eaee5630585bdad890869c2402c4343dcc
tommason14 <tom.mason14@gmail.com>
2020-06-19 09:35:32 +1000

    getboxsize

--- chem/lammps/getboxsize.sh
+++ chem/lammps/getboxsize.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-[[ $# -eq 0 || $1 == '-h' ]] && echo "Syntax: $(basename $0) traj.lmp" && exit 1
+[[ $# -eq 0 || $1 == '-h' ]] && echo "Syntax: $(basename $0)
+lammps_dump" && exit 1
 
 # all x components to average over, then find xhi - xlo and average
 sed -n '/ITEM: BOX/{n;p}' $1 | awk '{sum+=($2 - $1)} END {print sum/NR}'
