7485280aea861036b8197c3dcaa0aa1fc3dcbdce
tommason14 <tom.mason14@gmail.com>
2020-07-01 19:45:37 +1000

    kij

--- chem/pieda/pieda_make_fmo0.sh
+++ chem/pieda/pieda_make_fmo0.sh
@@ -20,7 +20,8 @@ do
   sed -i 's/RUNTYP=ENERGY/RUNTYP=FMO0/;s/NBODY=[0-9]//' "fmo0/$base.inp"
   # change fmoprp line- assumes one is already present; if not this script will fail!
   sed -i "s/\$FMOPRP.*/\$FMOPRP MAXAOC=$max_ao MODORB=3 MAXIT=200 \$END/" "fmo0/$base.inp"
-  # one node
+  # one node, normal queue
+  sed -i "/\#PBS -q.*/d" "fmo0/$base.job"
   sed -i "s/\#PBS -l ncpus=.*/\#PBS -l ncpus=48/" "fmo0/$base.job"
   sed -i "s/\#PBS -l mem=.*/\#PBS -l mem=192gb/" "fmo0/$base.job"
   sed -i "s/\#PBS -l jobfs=.*/\#PBS -l jobfs=200gb/" "fmo0/$base.job"
