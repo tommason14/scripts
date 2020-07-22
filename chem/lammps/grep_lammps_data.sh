c9c54f292e6e9ecc95dd0f7afb445661606eacff
tommason14 <tom.mason14@gmail.com>
2020-07-22 10:37:35 +1000

    modified to account for restarts

--- chem/lammps/grep_lammps_data.sh
+++ chem/lammps/grep_lammps_data.sh
@@ -19,34 +19,39 @@ fi
 # If log is not the first file, and rerun was started with a 
 # data file, the Step number starts at 0, when we want it to start at the
 # next timestep. So append to data.tmp sequentially.
+
+# find the header from first output
+# need the tail because minimisation
+# also prints out thermo data but with fewer columns
+first=$(find . -maxdepth 1 -name "lammps*out" | sort -n | head -1)
+grep "Step" "$first"| tail -1 | $sed 's/^\s\+//;s/\s\+$//;s/\s\+/,/g' > header.tmp
+
 [ -f data.tmp ] && rm data.tmp
 for f in $(find . -maxdepth 1 -name "lammps*out" | sort -n)
 do
-  lines="$(sed -n '/Step/,/Loop/p' $f |
+  # extract from "Setting up Verlet run..." onwards because 
+  # minimisation is finished by that point
+  lines="$(sed -n '/Setting up Verlet run .../,/Loop/p' $f |
     grep -v 'Step\|Loop' |
     grep '^\s\+[0-9]\+\s\+-*[0-9]\+' |
     $sed 's/^\s\+//;s/\s\+$//;s/\s\+/,/g')"
+  # if more than one file
   [ -f data.tmp ] && [ $(cat data.tmp | wc -l) -gt 0 ] && (
   # take last two lines and find step increment
-  laststep="$(tail -n 1 data.tmp | awk -F "," '{print $1}'| $sed 's/^\s*//;s/\s*$//')"
-  eqn="$(tail -n 2 data.tmp |
-    awk -F"," '{print $1}' |
-    $sed '/^\s*$/d' |
-    tr '\n' '-'|
-    $sed 's/\-$//')"
-    increment="$(echo "$eqn" | bc | $sed 's/^-//')"
-    echo "$lines" |
-    awk -F"," -v last="$laststep" -v inc="$increment" '$1+=last+inc' OFS="," >> data.tmp
+  laststep="$(tail -n 1 data.tmp | cut -d, -f1 | $sed 's/^\s*//;s/\s*$//')"
+  increment="$(tail -n 2 data.tmp | cut -d, -f1 | tr '\n' ' ' | $sed 's/\s*$//;s/ / - /' | bc | $sed 's/^-//')"
+  # If run was from a restart, timesteps continue from last run.
+  # check first line of new data- first timestep will be the same as the last
+  # of the previous run
+  first_of_new_run="$(echo $lines | head -1 | cut -d, -f1 | $sed 's/^\s*//;s/\s*$//')" 
+  [ "$first_of_new_run" = "$laststep" ] &&
+    # continue from restart
+    # density of 0th step is the same as the end of the last run, so drop the first line 
+    echo "$lines" | tail -n +2 | awk -F"," -v inc="$increment" '$1+=inc' OFS="," >> data.tmp ||
+    # adding on a new run that starts from 0, so add on last timestep
+    echo "$lines" | tail -n +2 | awk -F"," -v last="$laststep" -v inc="$increment" '$1+=last+inc' OFS="," >> data.tmp 
   ) || printf "%s\n" "$lines" > data.tmp
 done
 
-# find the header from first output
-first=$(find . -maxdepth 1 -name "lammps*out" | sort -n | head -1)
-ls "$first" |
-  sort -n |
-  head -1 |
-  xargs grep Step |
-  $sed 's/^\s\+//;s/\s\+$//;s/\s\+/,/g' > header.tmp
 cat header.tmp data.tmp 
-
 rm header.tmp data.tmp
