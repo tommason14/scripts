da8b737dcff4c3865b9d07bdb8343302687caa2f
tommason14 <tom.mason14@gmail.com>
2020-07-30 18:20:45 +1000

    travis

---
+++ chem/lammps/travis/rm_drudes_from_dump.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+[[ $# -eq 0 ]] &&
+echo "Remove drude particles from a lammps trajectory file
+and adjusts atom numbers accordingly.
+Produces a file named no_drude_filename.
+
+Syntax: $(basename $0) filename" && exit 1
+
+# print first timestep by taking lines between the first two 
+# occurrences of TIMESTEP
+num_atoms_no_drudes=$(
+  awk -v "f=0" '/TIMESTEP/{f=1;++d}d!=2{print}f&&d==2{print;exit}' $1 |
+  grep -v 'ITEM' | grep '^\s*[A-Z]' | grep -v '^\s*D' | wc -l)  
+
+orig_atoms=$(sed '/BOX BOUNDS/q' $1 | tail -n 2 | head -1)
+
+sed "s|$orig_atoms.*|$num_atoms_no_drudes|" $1 | grep -v "^\s*D" > no_drude_"$1"
---
+++ chem/lammps/travis/travis_show_msd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# remove colour codes and print diffusion info from travis log file
+# obtained by travis ... > file
+# IMPORTANT: sed expression is mac os (bsd) only
+
+sed -E "s/"$'\E'"\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" "${1:-/dev/stdin}" | sed -n '/Performing/,/assuming/p'
