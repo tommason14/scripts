61559d430127a4d1d236a9e39d48ab89b98ff65d
tommason14 <tom.mason14@gmail.com>
2020-07-31 13:23:41 +1000

    bonds made

--- chem/lammps/polymatic/bonds_made.sh
+++ chem/lammps/polymatic/bonds_made.sh
@@ -4,5 +4,8 @@
 echo "Counts the number of bonds the modified Polymatic code makes.
 Syntax $(basename $0) polymatic.out" && exit 1
 
-[[ $USER =~ (tmas0023|tommason) ]] && grep='ggrep' || grep='grep'
-$grep -o "([a-z].*)$" "$1" | sed 's/[\(\)]//g' | sort | uniq -c
+# [[ $USER =~ (tmas0023|tommason) ]] && grep='ggrep' || grep='grep'
+# $grep -o "([a-z].*)$" "$1" | sed 's/[\(\)]//g' | sort | uniq -c
+
+# cross-compatibility
+grep "Pair" "$1" | sed 's/^\s\+//' | cut -d ' ' -f 5- | sed 's/[\(\)]//g' | sort | uniq -c
