0f4220b0786d0396915835b0db9ace080b392569
tommason14 <tom.mason14@gmail.com>
2020-10-09 10:03:06 +1100

    lmp_gaff

--- chem/lammps/create_gaff_jobs/lmp_gaff.py
+++ chem/lammps/create_gaff_jobs/lmp_gaff.py
@@ -70,7 +70,7 @@ elif user == "tmas0023":
     vmd = "/Applications/VMD\ 1.9.3.app/Contents/vmd/vmd_MACOSXX86"
 else:
     vmd = "vmd"  # module loaded
-cmd = f"{vmd} -dispdev none -m topo-in.xyz -e tempfile"
+cmd = f"{vmd} -dispdev none -m topo-in.xyz -e tempfile 2>/dev/null" # warnings on gadi
 sp.check_output(cmd, shell=True)
 
 ### SUBSTITUTE IN ORIG NAMES ------------------------------
