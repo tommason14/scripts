39ef03c5cbd347dd390fde08c87b0866de91ba4a
tommason14 <tom.mason14@gmail.com>
2020-10-16 09:23:47 +1100

    getboxsize now finds the volume column

--- chem/lammps/getboxsize.sh
+++ chem/lammps/getboxsize.sh
@@ -2,7 +2,9 @@
 
 [[ $# -eq 0 || $1 == '-h' ]] &&
 echo "Finds box size by averaging last 30 timesteps printed, cube-rooting the volume.
-Assumes volume is the second to last column of the thermo output.
 Syntax: $(basename $0) lammps.out" && exit 1
 
-grep '^\s\+[0-9]\+\s\+[0-9]\+' $1 | tail -30 | awk '{vol+=$(NF-1)} END {print (vol/NR)**(1/3)}'
+# Find volume column
+col=$(grep Step $1 | tail -1 | tr ' ' '\n' | nl | grep Volume | awk '{print $1}')
+
+grep '^\s\+[0-9]\+\s\+[0-9]\+' $1 | tail -30 | awk -v col=$col '{vol+=$col} END {print (vol/NR)**(1/3)}'
