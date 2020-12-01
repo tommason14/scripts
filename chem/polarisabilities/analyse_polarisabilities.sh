568298deea758f6db3a8354cd8cc20fa228a709e
tommason14 <tom.mason14@gmail.com>
2020-12-01 13:55:59 +1100

    error out if missing file

--- chem/polarisabilities/analyse_polarisabilities.sh
+++ chem/polarisabilities/analyse_polarisabilities.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 field="${1:-0.0008}" # by default, 0.0008 au field strength assumed
 # numpy linear algebra deprecation warning is sent to /dev/null
+[[ ! -f connected.in ]] &&
+echo "Run $(dirname $0)/make_connected.in.py to create a file with bonding information" &&
+echo "and place this in the current directory." &&
+exit 1
 echo -e "yes\nyes\n$field\nyes\n" | python3 $(dirname $0)/analyse.py 2>/dev/null | sed -n '/Total polarizability:/,/-------/p' > polarisability.out
