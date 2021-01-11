42f386e403618f0a370e973cf18e24866dc34614
Thomas Mason <tm3124@gadi-login-08.gadi.nci.org.au>
2021-01-11 18:32:25 +1100

    fixed bugs

--- chem/pieda/pieda_boltz_kij.py
+++ chem/pieda/pieda_boltz_kij.py
@@ -10,11 +10,12 @@ import os
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 if len(sys.argv) == 2 and sys.argv[1] == '-h':
-    print(f"""Plot total energies against k_ij by typing:
-$ process_pieda.py --plot
-Otherwise a Boltzmann-averaged k_ij value is produced, weighted by the total energy of each system.
+    print(f"""Produces a Boltzmann-averaged k_ij value, weighted by the total energy of each system.
 Run {cwd}/fmo3_energies.sh beforehand to generate a csv with FMO3
-energies (fmo3.csv) that is used by this script.""")
+energies (fmo3.csv) that is used by this script.
+A plot of total energy against k_ij is saved as kij_vs_fmo3.png,
+but to view the plot in a popup window, type:
+$ pieda_boltz_kij.py --show""")
     sys.exit(1)
 
 
@@ -40,38 +41,34 @@ def boltz(diffs):
     numerator = exp((-1 * kJ_to_J * diffs) / (R * T))
     return numerator / numerator.sum()
 
-
-def plot_graph(df):
-    sns.set(style='white', font='Helvetica')
+def plot_graph(df, show_fig = False):
+    sns.set(style='white')
+    plt.rcParams['mathtext.default'] = 'regular'
     sns.scatterplot(x='diffs', y='k_ij', data=df)
     plt.xlabel(r'$\Delta$E$_{Tot}$ (kJ mol$^{-1}$)')
     plt.ylabel(r'k$_{ij}$')
-    plt.savefig('energies_v_kij.png',
-                dpi=300,
-                width=5,
-                height=4,
-                bbox_inches='tight')
-    plt.show()
-    print('saved image as energies_v_kij.png')
+    fig = plt.gcf()
+    fig.set_size_inches(5, 4)
+    plt.tight_layout()
+    plt.savefig('kij_vs_fmo3.png', dpi=300, bbox_inches='tight')
+    print('Plot of kij against energy saved as kij_vs_fmo3.png')
+    if show_fig:
+        plt.show()
 
-
-def main(plot=False):
+def main(show_fig=False):
     H_to_kJ = 2625.5
     kij = k_ij_values('results.txt')
     energies = pd.read_csv('fmo3.csv')
     energies.Path = energies.Path.str.replace('\./', '')
     df = energies.merge(kij, on='Path')
     df['diffs'] = (df['MP2/SRS'] - df['MP2/SRS'].min()) * H_to_kJ
-    if plot:
-        plot_graph(df)
-    else:
-        df['weights'] = boltz(df['diffs'])
-        print(f'Boltzmann averaged value = {(df.weights * df.k_ij).sum():.3f}')
-
+    plot_graph(df, show_fig)
+    df['weights'] = boltz(df['diffs'])
+    print(f'Boltzmann averaged value = {(df.weights * df.k_ij).sum():.3f}')
 
 if __name__ == "__main__":
-    try:
-        plot = sys.argv[1] == '--plot'
-    except IndexError:
-        plot = False
-    main(plot)
+    if len(sys.argv) > 1:
+        show_fig = sys.argv[1] == '--show'
+    else:
+        show_fig = False
+    main(show_fig)
--- chem/pieda/pieda_fmo3_energies.sh
+++ chem/pieda/pieda_fmo3_energies.sh
@@ -2,4 +2,4 @@
 
 autochem -r # collects all energies
 # now remove fmo0/free-state-polarisation/full-pieda lines
-head -1 energies.csv > fmo3.csv && grep 'cluster-[0-9],' energies.csv >> fmo3.csv
+head -1 energies.csv > fmo3.csv && grep 'cluster-[0-9]\+,' energies.csv >> fmo3.csv
