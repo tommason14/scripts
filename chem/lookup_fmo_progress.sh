5b1e9610076816f5adbf2d63a3625fd10fba2118
tommason14 <tom.mason14@gmail.com>
2021-01-14 12:39:49 +1100

    filename check

--- chem/lookup_fmo_progress.sh
+++ chem/lookup_fmo_progress.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
-then
-echo "Error: pass in filename"
-exit 1
-fi
+[ $# -ne 1 ] && echo "Error: pass in filename" && exit 1
+[ ! -f $1 ] && echo "$1 doesn't exist" && exit 1
 
 echo "Monomers -> $(grep iFrag $1 | sed '/jFrag/d' | wc -l)" 
 echo "Dimers -> $(grep jFrag $1 | sed '/kFrag/d' | wc -l)" 
