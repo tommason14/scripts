9fc9d47853bccddbeb750bc96c70e6599a4a0e8e
tommason14 <tom.mason14@gmail.com>
2021-01-22 14:35:50 +1100

    process the raw data produced by k_ij.py

---
+++ chem/pieda/pieda_process_centred.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from funcs import boltz
+import sys
+
+if len(sys.argv) != 2 or sys.argv[1] == '-h':
+    print('Takes raw_data.csv produced by k_ij.py '
+          '(https://github.com/tommason14/scripts/blob/master/chem/pieda/gadi/k_ij.py) '
+          'and plots k_ij against the difference in total energy of each cation or anion '
+          'centred configuration. This script also finds mean and Boltzmann-weighted average '
+          'k_ij parameters for each centred ion, and writes to a file named average_results.csv.\n'
+          'Syntax: pieda_process_centred.py filename.png')
+    sys.exit(1)
+
+H_to_kJ = 2625.5
+
+df = pd.read_csv('raw_data.csv')
+df['centred'] = df['Path'].str.split('/').str[1].str.split(
+    '-').str[:2].str.join(' ').str.capitalize()
+df['diffs'] = df.groupby('centred')['MP2/SRS'].transform(
+    lambda x: (x - x.min()) * H_to_kJ)
+df['weights'] = df.groupby('centred')['diffs'].transform(lambda x: boltz(x))
+sns.set(style='white')
+plt.rcParams['mathtext.default'] = 'regular'
+sns.lmplot(x='diffs',
+           y='k_ij',
+           hue='centred',
+           palette='Dark2',
+           data=df,
+           legend=False,
+           ci=None)
+plt.xlabel(r'$\Delta$E$_{Tot}$ (kJ mol$^{-1}$)')
+plt.ylabel(r'k$_{ij}$')
+plt.xlim(df['diffs'].min() - 10, df['diffs'].max() + 10) # extend x axis so that points aren't cut off
+plt.legend()
+plt.savefig(sys.argv[1], dpi=300, bbox_inches='tight')
+
+bw = df.groupby('centred').apply(lambda g: (g['weights'] * g['k_ij']).sum()
+                                 ).reset_index(name='boltz_weighted')
+means = df.groupby('centred')['k_ij'].mean().reset_index(name='mean')
+merged_means = bw.merge(means, on='centred')
+merged_means = merged_means.rename(columns={'centred': 'grouping'})
+no_groupby = df.drop(['diffs', 'weights'], axis=1).copy()
+no_groupby['diffs'] = (no_groupby['MP2/SRS'] - no_groupby['MP2/SRS'].min()) * H_to_kJ
+no_groupby['weights'] = boltz(no_groupby['diffs'])
+
+bw_ = (no_groupby['weights'] * no_groupby['k_ij']).sum()
+mean_ = no_groupby['k_ij'].mean()
+merged_means.append(
+    {
+        'grouping': 'all configs',
+        'boltz_weighted': bw_,
+        'mean': mean_
+    },
+    ignore_index=True).to_csv('average_results.csv', index=False)
