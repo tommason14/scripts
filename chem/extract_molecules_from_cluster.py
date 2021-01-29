8a3cd1fdceba9b15d80cb8df30028896a8f8fecb
tommason14 <tom.mason14@gmail.com>
2021-01-29 11:32:51 +1100

    run from current dir

--- chem/extract_molecules_from_cluster.py
+++ chem/extract_molecules_from_cluster.py
@@ -8,13 +8,15 @@ import sys
 base = os.path.basename(__file__)
 
 if len(sys.argv) != 2 or sys.argv[1] == '-h':
-    print('Extract all ions of a particular name from all xyz files in the parent directory.')
-    print(f'i.e. `{base} dhp` pulls out all dhp anions from xyz in the directory above.')
+    print('Extract all ions of a particular name from all xyz files in the current directory.')
+    print(f'i.e. `{base} dhp` pulls out all dhp anions from all xyz files and place them in a subdirectory')
     print(f'Syntax: {base} molecule')
     sys.exit(1)
 
-for f in glob('../*xyz'):
-    newname = f.replace('../', '')
+os.mkdir('extracted')
+
+for f in glob('*xyz'):
     mol = Molecule(f)
+    newname = os.path.join(os.getcwd(), 'extracted', f)
     extracted = [atom for atom in mol.coords if sys.argv[1] in atom.fragment]
     write_xyz(extracted, newname) 
