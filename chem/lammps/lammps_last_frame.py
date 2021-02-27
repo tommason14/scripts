674f454e3392d07e081abfd774a538ba9faef29b
Tom Mason <tommason14@gmail.com>
2021-02-27 12:42:17 +1100

    accept xyz filename

--- chem/lammps/lammps_last_frame.py
+++ chem/lammps/lammps_last_frame.py
@@ -2,11 +2,20 @@
 import sys
 from ovito.io import import_file, export_file
 
-if len(sys.argv) != 2 or sys.argv[1] == '-h':
-    print('Syntax: lammps_last_frame.py lammps_dump')
+if len(sys.argv) not in [2, 3] or sys.argv[1] == "-h":
+    print("Syntax: lammps_last_frame.py lammps_dump [output]")
     sys.exit(1)
+
+try:
+    output = sys.argv[2]
+except IndexError:
+    output = "last_frame.xyz"
 
 data = import_file(sys.argv[1])
 last = data.compute(data.source.num_frames)
-export_file(last, 'last_frame.xyz', 'xyz',
-columns=['Particle Type', 'Position.X', 'Position.Y', 'Position.Z'])
+export_file(
+    last,
+    output,
+    "xyz",
+    columns=["Particle Type", "Position.X", "Position.Y", "Position.Z"],
+)
