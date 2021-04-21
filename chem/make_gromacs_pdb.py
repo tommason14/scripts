b1e860a244064f773367d2ccd88c8a225ef1195c
Tom Mason <tommason14@gmail.com>
2021-04-21 17:36:33 +1000

    gmx pdb

--- chem/make_gromacs_pdb.py
+++ chem/make_gromacs_pdb.py
@@ -5,17 +5,24 @@ import sys
 
 
 def writepackmol(mols, files, box):
-    lines = ['tolerance 2.0', 'filetype pdb', 'output pack.pdb', '']
+    lines = [
+        "tolerance 2.0",
+        "filetype pdb",
+        "output pack.pdb",
+        "add_amber_ter",
+        "add_box_sides 1.0",
+        "",
+    ]
 
     for n, pdb in zip(mols, files):
-        lines += [f'structure {pdb}']
-        lines += [f'  number {n}']
-        lines += [f'  inside box 0 0 0 {box} {box} {box}']
-        lines += ['end structure']
-        lines += ['']
-    with open('pack.inp', 'w') as f:
+        lines += [f"structure {pdb}"]
+        lines += [f"  number {n}"]
+        lines += [f"  inside box 0 0 0 {box} {box} {box}"]
+        lines += ["end structure"]
+        lines += [""]
+    with open("pack.inp", "w") as f:
         for line in lines:
-            f.write(f'{line}\n')
+            f.write(f"{line}\n")
 
 
 def add_box_dimensions(fname, box):
@@ -23,34 +30,38 @@ def add_box_dimensions(fname, box):
         inp = f.readlines()
     index = 0
     for ind, line in enumerate(inp):
-        if 'ATOM' in line:
+        if "ATOM" in line:
             index = ind
             break
     box = float(box)
     inp.insert(
-        index,
-        f"CRYST1   {box:.3f}   {box:.3f}   {box:.3f}  90.00  90.00  90.00 P 1\n"
+        index, f"CRYST1   {box:.3f}   {box:.3f}   {box:.3f}  90.00  90.00  90.00 P 1\n"
     )
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         for line in inp:
             f.write(line)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description=('Replicating the setup procedure in fftool. '
-                     'Pass in pdb files with residues labelled, '
-                     'along with a box size in Å'))
+        description=(
+            "Replicating the setup procedure in fftool. "
+            "Pass in pdb files with residues labelled, "
+            "along with a box size in Å"
+        )
+    )
 
-    parser.add_argument('-b', '--box', help='box length in Å')
+    parser.add_argument("-b", "--box", help="box length in Å")
     parser.add_argument(
-        'files',
-        nargs='+',
-        help=('n1 pdb1 [n2 pdb2 ...], where n_i is the number of molecules '
-              'and pdb_i is a pdb structure for one molecule'))
+        "files",
+        nargs="+",
+        help=(
+            "n1 pdb1 [n2 pdb2 ...], where n_i is the number of molecules "
+            "and pdb_i is a pdb structure for one molecule"
+        ),
+    )
     args = parser.parse_args()
-    if any(x is None
-           for x in (args.files, args.box)) or len(args.files) % 2 != 0:
+    if any(x is None for x in (args.files, args.box)) or len(args.files) % 2 != 0:
         parser.print_help()
         sys.exit()
 
@@ -58,8 +69,8 @@ def main():
     pdbs = args.files[1::2]
 
     writepackmol(nmols, pdbs, args.box)
-    os.system('packmol < pack.inp')
-    add_box_dimensions('pack.pdb', args.box)
+    os.system("packmol < pack.inp")
+    # add_box_dimensions("pack.pdb", args.box)
 
 
 if __name__ == "__main__":
