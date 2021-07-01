757e0a19d297a6fd912e8cd852f0f782cf6a1209
tommason14 <tom.mason14@gmail.com>
2021-07-01 14:54:09 +1000

    wrapped xyz

---
+++ chem/lammps/datafile_to_wrapped_xyz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+if not 2 <= len(sys.argv) <= 3:
+    sys.exit("Uses Ovito to read in a datafile and write an xyz.\n"
+             "Atom type labels must be present in the Masses section\n"
+             "i.e. 7 12.011 # C3\n"
+             "Syntax: datafile_to_wrapped_xyz.py input [output (optional)]")
+from ovito.io import import_file, export_file
+from ovito.modifiers import UnwrapTrajectoriesModifier
+
+traj = sys.argv[1]
+if len(sys.argv) == 2:
+    output = traj.rsplit('.')[0] + '.wrapped.xyz'
+else:
+    output = sys.argv[2]
+
+pipeline = import_file(traj, atom_style='full')
+export_file(
+    pipeline,
+    output,
+    'xyz',
+    columns=["Particle Type", "Position.X", "Position.Y", "Position.Z"])
