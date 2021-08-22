6e6dedd48182eddbb116a3ef52dcd05a92ae1f5e
Tom Mason <tommason14@gmail.com>
2021-08-22 11:09:43 +1000

    removed loops

--- chem/vmd_wrap.tcl
+++ chem/vmd_wrap.tcl
@@ -1,7 +1,2 @@
 package require pbctools
-set numFrames [molinfo top get numframes]
-for  {set i 1} {$i <= $numFrames} {incr i} {
-animate goto $i
-pbc set [pbc get -now]
-pbc wrap    
-}
+pbc wrap -center com -compound residue -all
