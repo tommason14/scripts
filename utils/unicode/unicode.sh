05a6d23556b646a2fbf77b89bbeb8e5bdddd3878
Tom Mason <tommason14@gmail.com>
2021-09-25 15:08:56 +1000

    some glyphs require more than one unicode character

--- utils/unicode/unicode.sh
+++ utils/unicode/unicode.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed 's/^U\+/\\u/' "$(dirname $0)/unicode.txt" | while read -r line; do echo -e $line ; done | fzf --color=bg+:-1 --preview "echo -e {} | sed 's/, /\n/' | sed 's/, /\n/'" | awk -F',' '{print $1}' | tr -d '\n' | pbcopy
+sed 's/U\+/\\u/g' "$(dirname $0)/unicode.txt" | while read -r line; do echo -e $line ; done | fzf --color=bg+:-1 --preview "echo -e {} | sed 's/, /\n/' | sed 's/, /\n/'" | awk -F',' '{print $1}' | tr -d '\n' | pbcopy
