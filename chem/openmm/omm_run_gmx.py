8c508375812dc9366218bc25fc21e4c5e792f57a
tommason14 <tom.mason14@gmail.com>
2021-10-19 23:16:58 +1100

    added minimisation and restraints via resnames

--- chem/openmm/omm_run_gmx.py
+++ chem/openmm/omm_run_gmx.py
@@ -55,6 +55,21 @@ def arguments():
         help="Scale atomic charges by a multiplication factor",
         type=float,
     )
+    parser.add_argument(
+        "-r",
+        "--restrain",
+        help=(
+            "Add position restraints to these residues. "
+            "Pass in a list of residue names separated by spaces"
+        ),
+        nargs="+",
+    )
+    parser.add_argument(
+        "-min",
+        "--minimise",
+        help="Minimise energy before simulation",
+        action="store_true",
+    )
     return parser.parse_args()
 
 
@@ -67,6 +82,7 @@ def gen_simulation(
     timestep=1,
     interval=10000,
     charge_factor=None,
+    restrain=None,
 ):
     # OpenMM parsers can't read virtual sites, so use parmed to be safe
     gro = load_file(grofile)
@@ -86,6 +102,20 @@ def gen_simulation(
     if press is not None:
         baro = MonteCarloBarostat(press * bar, temp * kelvin)
         system.addForce(baro)
+
+    if restrain is not None:
+        atoms_to_restrain = [
+            a.idx for a in top.atoms for x in restrain if a.residue.name == x
+        ]
+        force = CustomExternalForce("k*((x-x0)^2+(y-y0)^2+(z-z0)^2)")
+        force.addGlobalParameter("k", 5.0 * kilocalories_per_mole / angstroms ** 2)
+        force.addPerParticleParameter("x0")
+        force.addPerParticleParameter("y0")
+        force.addPerParticleParameter("z0")
+        for i in atoms_to_restrain:
+            force.addParticle(i, list(gro.positions[i]))
+        system.addForce(force)
+
     integrator = LangevinIntegrator(
         temp * kelvin, 1 / picosecond, timestep * femtoseconds
     )
@@ -105,8 +135,8 @@ def gen_simulation(
         simulation.context.setPositions(gro.positions)
         simulation.context.setVelocitiesToTemperature(temp * kelvin)
 
-    simulation.reporters.append(XTCReporter("output.xtc", interval))
-    simulation.reporters.append(CheckpointReporter("output.chk", interval))
+    simulation.reporters.append(XTCReporter("traj.xtc", interval))
+    simulation.reporters.append(CheckpointReporter("restart.chk", interval))
     simulation.reporters.append(
         StateDataReporter(
             sys.stdout,
@@ -136,7 +166,15 @@ def main():
         timestep=args.timestep,
         interval=args.interval,
         charge_factor=args.scalecharge,
+        restrain=args.restrain,
     )
+
+    if args.minimise:
+        sim.minimizeEnergy()
+        state = sim.context.getState(getPositions=True)
+        with open("min.pdb", "w") as f:
+            PDBFile.writeFile(sim.topology, state.getPositions(), f)
+
     sim.step(args.steps)
 
 
