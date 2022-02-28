5ed8efed016458b67ac74aba13d977050f2c5fb5
Tom Mason <tommason14@gmail.com>
2022-02-28 23:46:42 +1100

    writing pdb for atom names; fpdf as script for tmux

--- chem/lammps/lammps_to_gromacs_gaff/amber_to_gmx.py
+++ chem/lammps/lammps_to_gromacs_gaff/amber_to_gmx.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import parmed as pmd
+p = pmd.load_file('polymer.prmtop', xyz='polymer.pdb')
 
-amber = pmd.load_file('polymer.prmtop','polymer.inpcrd')
+pdb = pmd.load_file('polymer.pdb')
+for old, new in zip(p.atoms, pdb.atoms):
+    old.name = new.name
 
-amber.save('polymer.top')
-amber.save('polymer.gro')
+p.save('polymer.gro')
+p.save('polymer.top')
