41ec3ceec4b3d4a2cd284d83cacd7de768076f11
Tom Mason <tommason14@gmail.com>
2022-03-02 22:05:28 +1100

    added cheat sheet

---
+++ utils/cheat_sheet.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# read command is different in bash and zsh - using bash form here
+read -p "Enter language or core util: " first
+read -p "Enter topic (press enter if core util): " second
+
+if [[ $second ]] ; then
+  curl -s cht.sh/$first/${second/ /+} | less -R
+else
+  curl -s cht.sh/$first | less -R
+fi
