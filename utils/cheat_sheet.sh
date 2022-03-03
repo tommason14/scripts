c45e9933ad16e55ca507a9edc46f21faf83b9005
Tom Mason <tommason14@gmail.com>
2022-03-03 23:55:14 +1100

    mods

--- utils/cheat_sheet.sh
+++ utils/cheat_sheet.sh
@@ -5,7 +5,7 @@ read -p "Enter language or core util: " first
 read -p "Enter topic (press enter if core util): " second
 
 if [[ $second ]] ; then
-  curl -s cht.sh/$first/${second/ /+} | less -R
+  curl -s cht.sh/$first/${second// /+} | less -R
 else
   curl -s cht.sh/$first | less -R
 fi
