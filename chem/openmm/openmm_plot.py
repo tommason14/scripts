016b8998b414de14bc13ac8027837c629b6ea2e1
tommason14 <tom.mason14@gmail.com>
2022-04-21 17:37:30 +1000

    python version is faster than gnuplot for long simulations

---
+++ chem/openmm/openmm_plot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+
+def read(fname):
+    header = None
+    data = []
+    found = False
+    with open(fname) as f:
+        for line in f:
+            if "Step" in line:
+                found = True
+                header = [i.replace('"', "").replace("#", "") for i in line.split(",")]
+                continue
+            if found:
+                data.append(line.split(","))
+    return pd.DataFrame(data, columns=header).apply(lambda x: pd.to_numeric(x))
+
+
+def column_from_command_line_pipe(df):
+    """
+    If user pipes in a partial match to any column, return it.
+    For example:
+    echo dens | openmm_plot.py nvt.log will return 'Density (g/mL)'
+    """
+    if not sys.stdin.isatty():
+        user_input = sys.stdin.readline().split()
+        argument = user_input[0]
+        for col in df.columns:
+            if re.search(argument.lower(), col.lower()):
+                return col
+    return None
+
+
+def main():
+    logfile = sys.argv[1]
+    df = read(logfile)
+    column = column_from_command_line_pipe(df)
+    if column:
+        df.set_index("Step")[column].plot().set(xlabel="Step", ylabel=column)
+        plt.tight_layout()
+        plt.show()
+    else:
+        # ask user for column from df - omit Step (the first column)
+        choices = [i for i in df.columns[1:]]
+        print("Please select a column from the following list:")
+        for i, choice in enumerate(choices, 1):
+            print(f"{i}: {choice}")
+        choice = int(input("Enter number: ")) - 1  # -1 because of Step omission
+        df.set_index("Step")[choices[choice]].plot().set(
+            xlabel="Step", ylabel=choices[choice]
+        )
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- chem/polarisabilities/make_fchk.sh
+++ chem/polarisabilities/make_fchk.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 
 cwd=$(pwd); for f in $(find . -name spec.chk); do dir=$(dirname $f); [[ ! -f $dir/spec.fchk ]] && echo "$dir" && cd $dir && formchk spec.chk && cd $cwd; done
